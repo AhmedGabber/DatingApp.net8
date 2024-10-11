d4a8703fb4c0f7a7d572a92178f51892a5d15111
AhmedGabber <amgaberh@gmail.com>
2024-10-11 16:04:17 +0300

    End of section 15

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -22,6 +22,8 @@ public partial class AppUser
     public List<photo> Photos { get; set; } = [];
     public List<UserLike> LikedByUsers { get; set; }=[];
     public List<UserLike> LikedUsers { get; set; }=[];
+    public List<Message> MessagesSent { get; set; }=[];
+    public List<Message> MessagesReceived { get; set; }=[];
     
 
 
